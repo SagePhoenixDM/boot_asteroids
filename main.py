4ac216b5210a5b21ccf99a9f7dedb01bfbe4b421
SagePhoenixDM <alanjackson9@icloud.com>
2025-10-06 17:22:29 -0400

    created window with pygame

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 # importing packages
 import pygame
+from constants import *
 
+
+# defining main function
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    print('Starting Asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+
+    # creating GUI window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # creating game loop
+    while True:
+        # exiting game loop if screen closed
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        # creating black screen
+        screen.fill(color=(0, 0, 0))
+        # refreshing screen
+        pygame.display.flip()
 
 
-if __name__ == "__main__":
+# only allowing main to run if executed from CLI
+if __name__ == '__main__':
     main()
